8d47ae0d04d33b2018679115512bd917a05ce506
AAmboise <adrien.amboise@yahoo.fr>
2022-09-23 14:31:52 +0200

    create form

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/dist/output.css" rel="stylesheet">
+    <title>Fil-rouge</title>
+</head>
+<body>
+
+<form method="post">
+
+    <label for="nom">votre nom</label>
+    <input type="text">
+
+    <label for="prenom">votre prenom</label>
+    <input type="text">
+
+    <label for="email"> votre email</label>
+    <input type="email">
+
+    <label for="date">choisir une date</label>
+    <input type="date">
+    
+    <button>valider</button>
+</form>
+    
+</body>
+</html>
