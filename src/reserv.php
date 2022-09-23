99861dafa2fda594787d147f1738cc2f75ed7ec8
Ski Project <filliongregoire@gmail.com>
2022-09-23 14:33:41 +0200

    okokre,dlekngrg

--- src/reserv.php
+++ src/reserv.php
@@ -4,12 +4,19 @@ namespace Src;
 
 class Reservation
 {
-    public integer $timestamp;
-    public string $name;
-    public string $email;
+    private integer $timestamp; //date choisie
+    private string $name; // nom de la personne
+    private string $email; // email de la personne
 
+    public function __construct(array $data)
+    {
+        $this->timestamp = $data['timestamp'];
+        $this->name = $data['name'];
+        $this->email = $data['email'];
+    }
     public function SaveReservation() : bool
     {
-        //save reservation dans la database
+        $db = new \PDO('mysql:host=localhost;dbname=test', $user, $pass);
+        $db->query('INSERT INTO reservation (...)');
     }
 }
